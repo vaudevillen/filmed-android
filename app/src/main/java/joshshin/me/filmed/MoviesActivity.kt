7a78d804b4d7b49b0da3c5bf202d493bc5d980ac
vaudevillainous <jjshin85@gmail.com>
2018-04-15 18:11:53 -0700

    add layouts, add some dimens

--- app/src/main/java/joshshin/me/filmed/MoviesActivity.kt
+++ app/src/main/java/joshshin/me/filmed/MoviesActivity.kt
@@ -3,10 +3,10 @@ package joshshin.me.filmed
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.movies_activity)
     }
 }
