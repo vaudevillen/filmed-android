9b2f76b1d15763c9c91017d8dd4f3b530feaa654
vaudevillainous <jjshin85@gmail.com>
2018-06-01 19:14:54 -0700

    add custom scopes

---
+++ app/src/main/java/me/joshshin/filmed/dagger/scope/Fragment.kt
@@ -0,0 +1,11 @@
+package me.joshshin.filmed.dagger.scope
+
+import javax.inject.Scope
+
+/**
+ * Created by Josh Shin on 6/1/18
+ */
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Fragment
